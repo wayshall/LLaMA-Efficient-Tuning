260fe1d7a5c50659cdf5e0d4190b1a6bfa2f43e4
weishao zeng <weishao.zeng@gmail.com>
2023-10-10 09:37:59 +0800

    fix: add travel nanjiang data

--- src/convertExcelData.py
+++ src/convertExcelData.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-df = pd.read_excel('/Users/way/mydev/llm/llm-test/data/三地旅游训练整理2.0.xlsx')
+df = pd.read_excel('/Users/way/mydev/llm/llm-test/data/南疆旅游训练整理.xlsx')
 # how='all'：如果一行（或一列）中的所有元素都是NaN，则删除该行（或该列）
 # axis=0参数告诉dropna方法只在行上执行删除操作，因此只有空行会被删除，而空列会保留。
 df = df.dropna(how='all', axis=0)
@@ -11,4 +11,4 @@ def handle_null(value):
       return ''
   return value
 df['input'] = df['input'].apply(handle_null)
-df.to_json(orient='records',path_or_buf='/Users/way/mydev/llm/LLaMA-Efficient-Tuning/data/cloud-micro-travel.json',force_ascii=False)
+df.to_json(orient='records',path_or_buf='/Users/way/mydev/llm/LLaMA-Efficient-Tuning/data/cloud-micro-travel-nanjiang.json',force_ascii=False)

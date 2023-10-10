8f219488629a9449f797f1b58229ade4fdcdcfe9
weishao zeng <weishao.zeng@gmail.com>
2023-10-10 16:49:30 +0800

    fix: fix namjiang data

--- src/convertExcelData.py
+++ src/convertExcelData.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-df = pd.read_excel('/Users/way/mydev/llm/llm-test/data/南疆旅游训练整理.xlsx')
+df = pd.read_excel('/Users/way/mydev/llm/llm-test/data/南疆旅游训练整理2.xlsx')
 # how='all'：如果一行（或一列）中的所有元素都是NaN，则删除该行（或该列）
 # axis=0参数告诉dropna方法只在行上执行删除操作，因此只有空行会被删除，而空列会保留。
 df = df.dropna(how='all', axis=0)
